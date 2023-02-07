a3298e0a270f0c6e9c47a797132e6fac30eca8f4
tomcats123 <124669018+tomcats123@users.noreply.github.com>
2023-02-07 11:56:49 +0100

    Add files via upload

--- c.c
+++ c.c
@@ -4,11 +4,11 @@ int main()
 {
 int a;
 int b;
-int risultato a / b;
+
 printf("inserisci un numero a\n");
 scanf("%d", &a);
 printf("inserisci un numero b\n");
 scanf("%d", &b);
+int risultato = a / b;
 printf("divisione di a e b: %d\n", risultato);
-
 }
