98f9ed0fc1d4f130d4ed58250f050550320dd1f5
tomcats123 <124669018+tomcats123@users.noreply.github.com>
2023-02-08 12:09:34 +0100

    Create c10.c

---
+++ c10.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+    int main()
+    {
+        int x = 5;
+        int y = 10;
+        while (y > x)
+        {
+            printf("g\n");
+            x = x + 2;
+
+        }
+    }
