063f5401620e6be97c09bb5ae851e1cbf0ee2403
tomcats123 <124669018+tomcats123@users.noreply.github.com>
2023-02-08 14:44:33 +0100

    Create c12.c

---
+++ c12.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+    int main()
+    {
+      int a = 0;
+      int somma = 0;
+
+      while(a < 100)
+      {
+       somma = somma + a;
+       a = a + 2;
+      }
+      printf("%d\n", somma);
+        
+      }  
+    
+    
